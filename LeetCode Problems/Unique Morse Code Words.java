1e3f055df2faceffee1408688cbb647a326cbe12
Devasri A <devasri30anandhababu@gmail.com>
2026-07-03 10:01:42 +0530

    Create Unique Morse Code Words

---
+++ LeetCode Problems/Unique Morse Code Words.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        
+        String[] codes = {
+            ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."
+        };
+        String temp="";
+        Set<String> set=new HashSet<>();
+        for(String each:words){
+            temp="";
+            for(char ch:each.toCharArray())
+            temp+=codes[ch-'a'];
+            set.add(temp);
+        }
+        return set.size();
+    }
+}

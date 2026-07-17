626f47e7c6a28b2b47bb230077416a0b17e651cd
Devasri A <devasri30anandhababu@gmail.com>
2026-07-17 09:44:57 +0530

    Create Sorted GCD Pair Queries

---
+++ LeetCode Problems/Sorted GCD Pair Queries.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int max = 0;
+        for (int x : nums) max = Math.max(max, x);
+
+        int[] freq = new int[max + 1];
+        for (int x : nums) freq[x]++;
+
+        long[] gcdCount = new long[max + 1];
+
+        for (int g = max; g >= 1; g--) {
+            long cnt = 0;
+
+            for (int multiple = g; multiple <= max; multiple += g) {
+                cnt += freq[multiple];
+                gcdCount[g] -= gcdCount[multiple];
+            }
+
+            gcdCount[g] += cnt * (cnt - 1) / 2;
+        }
+
+        for (int i = 2; i <= max; i++)
+            gcdCount[i] += gcdCount[i - 1];
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            long q = queries[i];
+
+            int l = 1, r = max;
+            while (l < r) {
+                int mid = (l + r) / 2;
+
+                if (gcdCount[mid] > q)
+                    r = mid;
+                else
+                    l = mid + 1;
+            }
+
+            ans[i] = l;
+        }
+
+        return ans;
+    }
+}

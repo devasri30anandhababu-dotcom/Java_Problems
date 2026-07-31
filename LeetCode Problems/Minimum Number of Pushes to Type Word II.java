214715b720e7984cacd251a39398396a90c419d3
Devasri A <devasri30anandhababu@gmail.com>
2026-08-01 00:35:11 +0530

    Add solution for minimum pushes to type a word

---
+++ LeetCode Problems/Minimum Number of Pushes to Type Word II.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        // Count frequency
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // Sort ascending
+        Arrays.sort(freq);
+
+        int pushes = 0;
+        int position = 0;
+
+        // Traverse from highest frequency
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+
+            pushes += freq[i] * ((position / 8) + 1);
+            position++;
+        }
+
+        return pushes;
+    }
+}

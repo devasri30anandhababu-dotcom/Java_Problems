ab8c0f584caa2ef96bfeff61c4483be070e33596
Devasri A <devasri30anandhababu@gmail.com>
2026-08-05 14:58:40 +0530

    Add Solution class to remove suspicious methods

---
+++ LeetCode Problems/Remove Methods From Project.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] e : invocations) {
+            graph[e[0]].add(e[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(k);
+        suspicious[k] = true;
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int v : graph[u]) {
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    q.offer(v);
+                }
+            }
+        }
+
+        for (int[] e : invocations) {
+            if (!suspicious[e[0]] && suspicious[e[1]]) {
+                List<Integer> all = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    all.add(i);
+                }
+                return all;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
